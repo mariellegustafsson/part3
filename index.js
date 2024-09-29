d2b14d1f4498984d6fa6b1b5ebce2d830292d34b
mariellegustafsson <marielle.gust@gmail.com>
2024-09-29 22:10:41 +0300

    small changes again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const cors = require('cors')
-
+app.use(express.static('dist'))
 app.use(cors())
 
 morgan = require('morgan')
